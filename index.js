e54af2fe0dbf4678fb7f665643b4f277cd72b2a4
viviana <vivianasolizosinaga@gmail.com>
2024-10-21 20:41:00 -0300

    creando carpeta js

---
+++ index.js
@@ -0,0 +1,20 @@
+var listadejuegos = [
+    {
+        nombre: "Tekken 7",
+        img: [
+            "./img/tekken7.jpg"
+        ]
+    },
+    {
+        nombre: "Resident Evil 4",
+        img: [
+            "./img/re4.jpeg"
+        ]
+    },
+    {
+        nombre: "Grand theft auto 5",
+        img: [
+            "./img/gta5.jpg"
+        ]
+    }
+]
